d3501da80e721d342432d35668f7278d9fb0148e
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-11-16 19:18:04 +0200

    For10
    homework

---
+++ For10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int N = int.Parse(Console.ReadLine());
+
+            float otvet = 0;
+
+
+            for (float i = 1; N >= i; i++)
+            {
+                otvet = 1 / i + otvet;
+            }
+            Console.WriteLine(otvet);
+        }
+    }
+}
