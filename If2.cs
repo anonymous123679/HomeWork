0c3601780bb8bb7d38d5d050b8ca1f4aa871d32d
Andrey <andmor228@gmail.com>
2021-11-16 18:47:48 +0200

    ti gey

---
+++ If2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine(a + 1);
+            }
+            else
+            {
+                Console.WriteLine(a - 2);
+            }
+        }
+        
+    }
+}
