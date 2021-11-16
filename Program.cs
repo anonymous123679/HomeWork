06f7436a277f609d90463615a3eeb98e0f5c9568
Andrey <andmor228@gmail.com>
2021-11-16 18:38:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            int a = int.Parse(Console.ReadLine());
+            int ones = a % 10;
+            int decates = a / 10 % 10;
+            int hundreads = a / 100;
+
+
+
+
+            Console.WriteLine(ones != decates && decates != hundreads);
+        }
+        
+    }
+}
