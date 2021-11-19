8aa32b32d60e4a6cee8354dc79ba5854f0ad068d
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-11-19 19:06:45 +0200

    For11
    For11

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 
-namespace ConsoleApp7
+namespace ConsoleApp9
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число");
+            int N = int.Parse(Console.ReadLine());
 
-            int a = int.Parse(Console.ReadLine());
-            int ones = a % 10;
-            int decates = a / 10 % 10;
-            int hundreads = a / 100;
+            int otvet = 0;
 
 
+            for (int i = 1; N >= i; i++)
+            {
+                otvet = (N + i) * (N + i) + otvet;
+            }
+            Console.WriteLine(otvet + N * N + ((2 * N) * (2 * N)));
 
 
-            Console.WriteLine(ones != decates && decates != hundreads);
         }
-        
     }
 }
