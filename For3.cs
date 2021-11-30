c9004e36859221d7315da15863477821b827b0f0
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-11-30 23:09:34 +0200

    For3
    For3

---
+++ For3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = b - 1; i > a; i--)
+                Console.WriteLine(i);
+            Console.WriteLine(b - a - 1);
+        }
+    }
+}
