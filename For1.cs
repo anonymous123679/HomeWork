1e09e4e5c29db0c9f0d8b3d87ddb2bbf033c4020
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-11-30 22:25:17 +0200

    For1
    For1

---
+++ For1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int K;
+
+            Console.Write("Enter numbers: ");
+            N = int.Parse(Console.ReadLine());
+            K = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+
+            {
+                Console.WriteLine(K);
+            }
+        }
+    }
+}
