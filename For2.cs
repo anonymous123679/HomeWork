d9ee584b9b9e29ab48c8bd2c425a13bf19f748db
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-11-30 23:02:46 +0200

    For2
    For2

---
+++ For2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i <= b; i++) Console.WriteLine(i);
+                int n = b - a + 1;
+                Console.WriteLine(n);
+                Console.ReadKey();
+            }
+        }
+}
