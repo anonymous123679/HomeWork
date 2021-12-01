b51480bb4c1b575a2f83908ced0f7175c03e65b6
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 13:28:07 +0200

    For8
    For8

---
+++ For8.cs
@@ -0,0 +1,9 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int answer = 1;
+for (int i = A; i <= B; i++)
+{
+    answer *= i;
+    Console.WriteLine(answer);
+
+}
