f5a17c7f98d7b3f3f9f3353bc8a7650326560330
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 12:55:16 +0200

    For5
    For5

---
+++ For5.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+float L = 1;
+float N;
+int kg;
+Console.Write("Enter numbers: ");
+N = int.Parse(Console.ReadLine());
+if (N < 0)
+{
+
+    Console.WriteLine("vi debil");
+    return;
+}
+for (kg = 9; kg > 0; L++)
+   
+{
+    Console.WriteLine(N * L * 0.1);
+    if (kg < L)
+    {
+        Console.ReadKey();
+        return;
+    }
+}
