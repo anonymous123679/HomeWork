dfafc7afc184f59b4b49b6b84c7f1e37a5b25f65
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 11:35:18 +0200

    For4
    For4

---
+++ For4.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int L = 1;
+int N;
+int kg;
+Console.Write("Enter numbers: ");
+N = int.Parse(Console.ReadLine());
+for (kg = 9; kg > 0; L++)
+
+{
+    Console.WriteLine(N * L);
+    if (kg < L)
+    {
+        Console.ReadKey();
+        return;
+    }
+} 
