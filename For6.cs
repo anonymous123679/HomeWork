b8427cf9de983880a367bbb4b4890f4a49700569
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 12:54:49 +0200

    For6
    For6

---
+++ For6.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+float L = 0;
+int N;
+float kg;
+Console.Write("Enter numbers: ");
+N = int.Parse(Console.ReadLine());
+for (kg = 1.2f; kg < 2.1; kg += 0.2f, L++)
+
+{
+    Console.WriteLine(kg * N);
+    if (L == 4)
+    {
+        Console.ReadKey();
+        return;
+    }
+}
