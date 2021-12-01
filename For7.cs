5597d1b2b28f278fa7393ab171e6e4facd05d19b
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 13:19:58 +0200

    For7
    For7

---
+++ For7.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int answer = 0;
+for (int i = A; i <= B; i++)
+{
+    answer += i;
+    Console.WriteLine(answer);
+
+}
