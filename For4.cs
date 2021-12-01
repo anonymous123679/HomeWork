bf8a23a86058c4ea002885af2b2a6b9b9e3c00b3
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 19:14:33 +0200

    For4
    For4

--- For4.cs
+++ For4.cs
@@ -3,7 +3,7 @@ int N;
 int kg = 10;
 Console.Write("Enter numbers: ");
 N = int.Parse(Console.ReadLine());
-for (int i = 0; kg >= i; i++)
+for (int i = 0; kg <= i; i++)
 {
     Console.WriteLine(N * i);
 }
