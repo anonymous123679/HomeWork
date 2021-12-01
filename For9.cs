c25d9cee21d603ef0e0455c603f53d7dccced8c8
pagiloi drakon <94472548+anonymous123679@users.noreply.github.com>
2021-12-01 17:53:54 +0200

    For9
    For9

---
+++ For9.cs
@@ -0,0 +1,8 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int answer = 1;
+for (int i = A; i <= B; i++)
+{
+    answer *= i * i;
+    Console.WriteLine(answer);
+}
